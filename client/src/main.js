50c6ce4d6972dffa5f5cb46cb762d3a675e9d355
Hafizh <hafizh.rafianaji@gmail.com>
2020-09-18 14:20:34 +0700

    ready to deploy

--- client/src/main.js
+++ client/src/main.js
@@ -8,7 +8,7 @@ import VueSocketIO from 'vue-socket.io'
 
 Vue.use(new VueSocketIO({
   debug: true,
-  connection: 'http://localhost:3000',
+  connection: 'https://kaheight-group-project.herokuapp.com',
   vuex: {
     store,
     actionPrefix: 'SOCKET_',
